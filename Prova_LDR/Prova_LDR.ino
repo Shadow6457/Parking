90ed60989dc5891fea9d7f9c48ee0a728ed803ea
Iker Clemente <ikercp0@gmail.com>
2024-04-03 19:31:12 +0200

    Create Prova_LDR.ino

---
+++ Prova_LDR/Prova_LDR.ino
@@ -0,0 +1,36 @@
+
+const int LDR1 = A1;
+const int LDR2 = A2;
+const int LDR3 = A3;
+const int LDR4 = A0;
+
+//Variables 
+int Valor_LDR1, Valor_LDR2, Valor_LDR3, Valor_LDR4;
+
+void setup() 
+{
+  pinMode(LDR1, INPUT);
+  pinMode(LDR2, INPUT);
+  pinMode(LDR3, INPUT);
+  pinMode(LDR4, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Valor_LDR1=analogRead(LDR1);
+  Valor_LDR2=analogRead(LDR2);
+  Valor_LDR3=analogRead(LDR3);
+  Valor_LDR4=analogRead(LDR4);
+
+  Serial.print("LDR1 = ");
+  Serial.println(Valor_LDR1);
+  Serial.print("LDR2 = ");
+  Serial.println(Valor_LDR2);
+  Serial.print("LDR3 = ");
+  Serial.println(Valor_LDR3);
+  Serial.print("LDR4 = ");
+  Serial.println(Valor_LDR4);
+  delay(2000);
+
+}
